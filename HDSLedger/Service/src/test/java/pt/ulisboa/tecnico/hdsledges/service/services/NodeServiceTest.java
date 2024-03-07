8dd3d5544911c2d573b14593ae716c40c6460761
tildeeine <t1ld3me@gmail.com>
2024-03-07 22:56:52 +0000

    feat: Added basic running tests

--- HDSLedger/Service/src/test/java/pt/ulisboa/tecnico/hdsledges/service/services/NodeServiceTest.java
+++ HDSLedger/Service/src/test/java/pt/ulisboa/tecnico/hdsledges/service/services/NodeServiceTest.java
@@ -36,6 +36,7 @@ public class NodeServiceTest {
     private static String clientsConfigPath = "src/test/resources/client_test_config.json";
     private static String nodeId = "2";
     private static String leaderId = "1";
+    private static String clientId = "client1";
 
     // Creating mock objects
     @Mock
@@ -87,14 +88,12 @@ public class NodeServiceTest {
     // Test that the uponPrePrepare method sends a PREPARE message upon receiving a
     // PRE-PREPARE message from the leader
     @Test
-    public void test_uponPrePrepare_sendsPrepareUponReceivingLeader() {
+    public void test_uponPrePrepare_leader() {
         // Mock that sender is leader
         when(mockMessage.getSenderId()).thenReturn(leaderId);
         when(mockMessage.deserializePrePrepareMessage()).thenReturn(mockPrePrepareMessage);
         when(mockPrePrepareMessage.getValue()).thenReturn("val");
         // Call method
-        System.out.println(leaderNodeService.getLink());// ! testing
-
         leaderNodeService.uponPrePrepare(mockMessage);
 
         // Verify that uponPrePrepare broadcasts a message
@@ -108,140 +107,53 @@ public class NodeServiceTest {
         }));
     }
 
-    // // Test that the uponPrePrepare method does not send a PREPARE message upon
-    // // receiving PRE-PREPARE message from a non-leader
-    // @Test
-    // public void test_uponPrePrepare_doesNotSendPrepareUponReceivingNonLeader() {
-    // // Mock that sender is not leader
-    // when(mockMessage.getSenderId()).thenReturn(nodeId);
+    // Test that the uponPrePrepare method does not send a PREPARE message upon
+    // receiving PRE-PREPARE message from a non-leader
+    @Test
+    public void test_uponPrePrepare_nonLeader() {
+        // Mock that sender is not leader
+        when(mockMessage.getSenderId()).thenReturn(nodeId);
+        when(mockMessage.deserializePrePrepareMessage()).thenReturn(mockPrePrepareMessage);
+        when(mockPrePrepareMessage.getValue()).thenReturn("val");
 
-    // // Call method
-    // nodeService.uponPrePrepare(mockMessage);
+        // Call method
+        nodeService.uponPrePrepare(mockMessage);
 
-    // // Verify that uponPrePrepare does not broadcast a message
-    // verify(mockLink, times(0)).broadcast(any());
-    // }
+        // Verify that uponPrePrepare does not broadcast any message
+        verify(mockNodeLink, times(0)).broadcast(any());
+    }
 
-    // // Test that startConsensus initiates a new
-    // // consensus instance, updates the relevant internal data structures, and
-    // // broadcasts the necessary messages when the node is the leader.
-    // @Test
-    // public void test_startConsensus_leader() {
-    // // Mock that sender is leader
-    // when(mockMessage.getSenderId()).thenReturn(leaderId);
-    // // Mock that there has been no consensus instance for the round
-    // // when(nodeService.getInstanceInfo().put(Mockito.anyInt(),
-    // // any(InstanceInfo.class))).thenReturn(null); //! Can't mock nodeservice
-    // // // Mock that previous consensus instance has been decided
-    // // when(nodeService.getConsensusInstance().incrementAndGet()).thenReturn(2);
-    // // when(nodeService.getLastDecidedConsensusInstance()).thenReturn(1);
+    // Test that startConsensus initiates a new
+    // consensus instance, updates the relevant internal data structures, and
+    // broadcasts the necessary messages when the node is the leader.
+    @Test
+    public void test_handleClientRequest_leader() {
+        when(mockMessage.getSenderId()).thenReturn(clientId);
+        when(mockMessage.getValue()).thenReturn("val");
 
-    // // Call method
-    // nodeService.startConsensus("val");
+        // Call method
+        leaderNodeService.handleClientRequest(mockMessage);
 
-    // // Verify that startConsensus initiates a new consensus instance and
-    // broadcasts
-    // // PRE-PREPARE message
-    // verify(mockLink, times(1)).broadcast(argThat(argument -> {
-    // // Check if the broadcasted message is a PRE-PREPARE message
-    // if (argument instanceof ConsensusMessage) {
-    // ConsensusMessage prePrepareMessage = (ConsensusMessage) argument;
-    // return prePrepareMessage.getType() == Message.Type.PRE_PREPARE;
-    // }
-    // return false;
-    // }));
-    // }
+        // Verify that startConsensus broadcasts a PRE-PREPARE message
+        verify(mockLeaderLink, times(1)).broadcast(argThat(argument -> {
+            // Check if the broadcasted message is a PRE-PREPARE message
+            if (argument instanceof ConsensusMessage) {
+                ConsensusMessage prePrepareMessage = (ConsensusMessage) argument;
+                return prePrepareMessage.getType() == Message.Type.PRE_PREPARE;
+            }
+            return false;
+        }));
+    }
 
-    // // Test that startConsensus does not initiate a new consensus instance when
-    // the
-    // // sending node is not the leader.
-    // @Test
-    // public void test_startConsensus_notLeader() {
-    // // Mock that sender is not the leader
-    // when(mockMessage.getSenderId()).thenReturn(nodeId);
-    // // when(nodeService.getInstanceInfo().put(Mockito.anyInt(),
-    // // any(InstanceInfo.class))).thenReturn(null);
-    // // Mock that previous consensus instance has been decided //! Can not mock
-    // // nodeservice
-    // // when(nodeService.getConsensusInstance().incrementAndGet()).thenReturn(2);
-    // // when(nodeService.getLastDecidedConsensusInstance()).thenReturn(1);
+    // Test that handleClientRequest does not start a new consensus instance when
+    // the node is not the leader.
+    @Test
+    public void test_handleClientRequest_notLeader() {
+        // Call method
+        nodeService.handleClientRequest(mockMessage);
 
-    // // Call method
-    // nodeService.startConsensus("val");
-
-    // // Verify that startConsensus does not initiate a new consensus instance
-    // verify(mockLink, times(0)).broadcast(any());
-    // }
-
-    // // Test that startConsensus does not initiate a new consensus if there has
-    // // already been a consensus instance for the round
-    // @Test
-    // public void test_startConsensus_alreadyConsensusInstance() {
-    // // Mock that sender is leader
-    // when(mockMessage.getSenderId()).thenReturn(leaderId);
-    // // when(nodeService.isLeader("2")).thenReturn(true); // Mock that there has
-    // been
-    // // a consensus instance for the round //! Can't mock nodeservice
-    // // when(nodeService.getInstanceInfo().put(Mockito.anyInt(),
-    // // any(InstanceInfo.class)))
-    // // .thenReturn(new InstanceInfo("val"));
-
-    // // // Mock that previous consensus instance has been decided
-    // // when(nodeService.getConsensusInstance().incrementAndGet()).thenReturn(2);
-    // // when(nodeService.getLastDecidedConsensusInstance()).thenReturn(1);
-
-    // // Call method
-    // nodeService.startConsensus("val");
-
-    // // Verify that startConsensus does not initiate a new consensus instance
-    // verify(mockLink, times(0)).broadcast(any());
-    // }
-
-    // // Test that startConsensus does not initiate a new consensus if the previous
-    // // round was not decided yet
-
-    // // Test that handleClientRequest starts a new consensus instance when the
-    // // sending node is the leader, nad updates clientRequestID appropriately
-    // @Test
-    // public void test_handleClientRequest() {
-    // // Mock that we are the leader and client ID is 0
-    // // when(mockProcessConfig.isLeader()).thenReturn(true);
-    // when(mockMessage.getSenderId()).thenReturn("0");
-    // // Mock that there has been no consensus instance for the round
-    // // when(nodeService.getInstanceInfo().put(Mockito.anyInt(),
-    // // any(InstanceInfo.class))).thenReturn(null); //! Can't mock nodeservice
-    // // // Mock that previous consensus instance has been decided
-    // // when(nodeService.getConsensusInstance().incrementAndGet()).thenReturn(2);
-    // // when(nodeService.getLastDecidedConsensusInstance()).thenReturn(1);
-
-    // // Call method
-    // nodeService.handleClientRequest(mockMessage);
-
-    // // Verify that handleClientRequest starts a new consensus instance and
-    // updates
-    // // the clientRequestID
-    // verify(mockLink, times(1)).broadcast(argThat(argument -> {
-    // // Check if the broadcasted message is a PRE-PREPARE message
-    // if (argument instanceof ConsensusMessage) {
-    // ConsensusMessage prePrepareMessage = (ConsensusMessage) argument;
-    // return prePrepareMessage.getType() == Message.Type.PRE_PREPARE;
-    // }
-    // return false;
-    // }));
-    // }
-
-    // // Test that handleClientRequest does not start a new consensus instance when
-    // // the node is not the leader.
-    // @Test
-    // public void test_handleClientRequest_notLeader() {
-    // // Mock not the leader
-    // when(mockMessage.getSenderId()).thenReturn(nodeId);
-
-    // // Call method
-    // nodeService.handleClientRequest(mockMessage);
-
-    // // Verify that handleClientRequest does not start a new consensus instance
-    // verify(mockLink, times(0)).broadcast(any());
-    // }
+        // Verify that handleClientRequest does not start a new consensus instance
+        verify(mockNodeLink, times(0)).broadcast(any());
+    }
 
 }
